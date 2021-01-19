c46e6f7294bdada6f5bd080c1fce0052f9786617
Nurbek <tuzelnurbek@gmail.com>
2021-01-19 13:34:47 +0600

    create user controller

---
+++ src/users/schemas/user.model.ts
@@ -0,0 +1,37 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+    @Prop({required: true})
+    firstname: String
+
+    @Prop({required: true})
+    lastname: String
+
+    @Prop({required: true, unique: true})
+    email: String
+
+    @Prop({required: true})
+    password: String
+
+    @Prop({required: true})
+    access_token: String
+
+    @Prop()
+    create_user: String
+
+    @Prop()
+    update_user: String
+
+    @Prop({required: true, default: Date.now()})
+    create_date: Date
+
+    @Prop({required: true, default: Date.now()})
+    update_date: Date
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
