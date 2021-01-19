80119b8daa069b541cfd87f556ec5b614d0865af
Nurbek <tuzelnurbek@gmail.com>
2021-01-19 13:43:00 +0600

    connect mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 
+
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, MongooseModule.forRoot('mongodb+srv://app-flutter:nurka111222111@app.u0kg2.mongodb.net/app?retryWrites=true&w=majority')],
   controllers: [],
   providers: [],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,11 @@
 import { UsersController } from './users.controller';
+
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.model';
 
 @Module({
+    imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}])],
     controllers: [UsersController],
     providers: []
 })
