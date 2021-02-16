a9bdb9015a205fb0519571ccb97c53cd563df580
Nurbek <tuzelnurbek@gmail.com>
2021-02-16 23:52:17 +0600

    add product

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,12 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductController } from './product/product.controller';
 
 
 @Module({
   imports: [UsersModule, MongooseModule.forRoot('mongodb+srv://app-flutter:nurka111222111@app.u0kg2.mongodb.net/app?retryWrites=true&w=majority'), AuthModule],
-  controllers: [AppController, UsersController],
+  controllers: [AppController, UsersController, ProductController],
   providers: [AppService],
 })
 export class AppModule {}
