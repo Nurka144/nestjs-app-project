8ffaec939db39ebcd4e3b50f5e60cb1eec73a808
Nurbek <tuzelnurbek@gmail.com>
2021-02-18 23:51:39 +0600

    product

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -2,11 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { Product, ProductDocument } from './schemas/product.model';
 import { InjectModel } from '@nestjs/mongoose';
+import { ProductDto } from './dto/product.dto';
 
 @Injectable()
 export class ProductService {
 
     constructor(@InjectModel(Product.name) private ProductModel: Model<ProductDocument>) {}
-    
 
+    async saveProduct(productData: ProductDto): Promise<any> {
+        const createProductData = new this.ProductModel({
+            title: productData.title,
+            description: productData.description
+        })
+        return createProductData.save();
+    }
+
+    async findOne(productId: String): Promise<any> {
+        let findProduct = await this.ProductModel.find({_id: productId});
+        return findProduct;
+    }
 }
